55c8fa134dd35477301ce33c748a2c31ad2de10d
Shannon Poole <spoole@redhat.com>
2025-10-28 15:00:05 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/openshift-sustaining/arc-test-component-b
+
+go 1.19
+
+require github.com/golang/glog v1.1.2
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/golang/glog"
+)
+
+func main() {
+	glog.Error("Prepare to repel boarders")
+	glog.Flush()
+}
