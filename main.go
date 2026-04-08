6f8f06e347a5155b87b4804b5c6beea0550393b3
Venkata Charan Sunku <vsunku@redhat.com>
2026-04-08 19:30:33 +0530

    recreate CVE-2026-33186 for grpc

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"log"
+	"net"
+	"net/http"
+
 	"github.com/golang/glog"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	glog.Error("Prepare to repel boarders")
 	glog.Flush()
+
+	s := grpc.NewServer()
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	http.HandleFunc("/grpc", func(w http.ResponseWriter, r *http.Request) {
+		s.ServeHTTP(w, r)
+	})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
