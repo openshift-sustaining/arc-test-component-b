92ff6925fbd8be091e4f4462a54acf9b12ba8664
Little Monster <rjagadal@redhat.com>
2026-04-09 17:45:47 +0530

    add grpc v1.65.0 + .ci-operator.yaml (golang-1.21) for Go version handling tests

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	"github.com/golang/glog"
+	"google.golang.org/grpc"
 )
 
 func main() {
-	glog.Error("Prepare to repel boarders")
-	glog.Flush()
+	glog.Info("Starting server...")
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
